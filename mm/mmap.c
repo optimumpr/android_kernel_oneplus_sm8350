d4b6e03b9db239af389c62d02c6826b1c655a2bc
Lorenzo Stoakes <lstoakes@gmail.com>
2025-07-29 17:58:06 -0700

    mm: drop the assumption that VM_SHARED always implies writable
    [ Upstream commit e8e17ee90eaf650c855adb0a3e5e965fd6692ff1 ]
    
    Patch series "permit write-sealed memfd read-only shared mappings", v4.
    
    The man page for fcntl() describing memfd file seals states the following
    about F_SEAL_WRITE:-
    
        Furthermore, trying to create new shared, writable memory-mappings via
        mmap(2) will also fail with EPERM.
    
    With emphasis on 'writable'.  In turns out in fact that currently the
    kernel simply disallows all new shared memory mappings for a memfd with
    F_SEAL_WRITE applied, rendering this documentation inaccurate.
    
    This matters because users are therefore unable to obtain a shared mapping
    to a memfd after write sealing altogether, which limits their usefulness.
    This was reported in the discussion thread [1] originating from a bug
    report [2].
    
    This is a product of both using the struct address_space->i_mmap_writable
    atomic counter to determine whether writing may be permitted, and the
    kernel adjusting this counter when any VM_SHARED mapping is performed and
    more generally implicitly assuming VM_SHARED implies writable.
    
    It seems sensible that we should only update this mapping if VM_MAYWRITE
    is specified, i.e.  whether it is possible that this mapping could at any
    point be written to.
    
    If we do so then all we need to do to permit write seals to function as
    documented is to clear VM_MAYWRITE when mapping read-only.  It turns out
    this functionality already exists for F_SEAL_FUTURE_WRITE - we can
    therefore simply adapt this logic to do the same for F_SEAL_WRITE.
    
    We then hit a chicken and egg situation in mmap_region() where the check
    for VM_MAYWRITE occurs before we are able to clear this flag.  To work
    around this, perform this check after we invoke call_mmap(), with careful
    consideration of error paths.
    
    Thanks to Andy Lutomirski for the suggestion!
    
    [1]:https://lore.kernel.org/all/20230324133646.16101dfa666f253c4715d965@linux-foundation.org/
    [2]:https://bugzilla.kernel.org/show_bug.cgi?id=217238
    
    This patch (of 3):
    
    There is a general assumption that VMAs with the VM_SHARED flag set are
    writable.  If the VM_MAYWRITE flag is not set, then this is simply not the
    case.
    
    Update those checks which affect the struct address_space->i_mmap_writable
    field to explicitly test for this by introducing
    [vma_]is_shared_maywrite() helper functions.
    
    This remains entirely conservative, as the lack of VM_MAYWRITE guarantees
    that the VMA cannot be written to.
    
    Link: https://lkml.kernel.org/r/cover.1697116581.git.lstoakes@gmail.com
    Link: https://lkml.kernel.org/r/d978aefefa83ec42d18dfa964ad180dbcde34795.1697116581.git.lstoakes@gmail.com
    Signed-off-by: Lorenzo Stoakes <lstoakes@gmail.com>
    Suggested-by: Andy Lutomirski <luto@kernel.org>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: stable@vger.kernel.org
    Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/mmap.c
+++ mm/mmap.c
@@ -143,7 +143,7 @@ static void __remove_shared_vm_struct(struct vm_area_struct *vma,
 {
 	if (vma->vm_flags & VM_DENYWRITE)
 		atomic_inc(&file_inode(file)->i_writecount);
-	if (vma->vm_flags & VM_SHARED)
+	if (vma_is_shared_maywrite(vma))
 		mapping_unmap_writable(mapping);
 
 	flush_dcache_mmap_lock(mapping);
@@ -659,7 +659,7 @@ static void __vma_link_file(struct vm_area_struct *vma)
 
 		if (vma->vm_flags & VM_DENYWRITE)
 			atomic_dec(&file_inode(file)->i_writecount);
-		if (vma->vm_flags & VM_SHARED)
+		if (vma_is_shared_maywrite(vma))
 			atomic_inc(&mapping->i_mmap_writable);
 
 		flush_dcache_mmap_lock(mapping);
@@ -1871,7 +1871,7 @@ unsigned long mmap_region(struct file *file, unsigned long addr,
 			if (error)
 				goto free_vma;
 		}
-		if (vm_flags & VM_SHARED) {
+		if (is_shared_maywrite(vm_flags)) {
 			error = mapping_map_writable(file->f_mapping);
 			if (error)
 				goto allow_write_and_free_vma;
@@ -1909,7 +1909,7 @@ unsigned long mmap_region(struct file *file, unsigned long addr,
 	vma_link(mm, vma, prev, rb_link, rb_parent);
 	/* Once vma denies write, undo our temporary denial count */
 	if (file) {
-		if (vm_flags & VM_SHARED)
+		if (is_shared_maywrite(vm_flags))
 			mapping_unmap_writable(file->f_mapping);
 		if (vm_flags & VM_DENYWRITE)
 			allow_write_access(file);
@@ -1953,7 +1953,7 @@ unmap_and_free_vma:
 
 	/* Undo any partial mapping done by a device driver. */
 	unmap_region(mm, vma, prev, vma->vm_start, vma->vm_end);
-	if (vm_flags & VM_SHARED)
+	if (is_shared_maywrite(vm_flags))
 		mapping_unmap_writable(file->f_mapping);
 allow_write_and_free_vma:
 	if (vm_flags & VM_DENYWRITE)
